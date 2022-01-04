74bd2f80669d943a06a01acedd2a2bf3c66a94b3
Whytel Macro <daPiang@mcm.edu.ph>
2022-01-05 03:12:53 +0800

    cleanup

--- main.js
+++ main.js
@@ -1,73 +1,106 @@
-function getRizalQuote() {
-    if(buttonStateVar!=1){
-        quoteText.innerHTML = '';
+let bg = document.getElementById('bg_img');
+let person = document.getElementById('person_img');
+let quote = document.getElementById('quote_txt');
+let plate = document.getElementById('plate_txt');
+let rizal_btn = document.getElementById('btn-rizal');
+let mart_btn = document.getElementById('btn-mart');
+let sun_btn = document.getElementById('btn-sun');
+let soc_btn = document.getElementById('btn-soc');
+let niet_btn = document.getElementById('btn-niet');
+let quote_btn = document.getElementById('quote_btn');
+
+let buttonState = 0;
+let quoteState = -1;
+
+function setRizal() {
+    if(buttonState!=1){
+        quote.innerHTML = '';
     }
 
-    buttonStateVar = 1;
-    quoteState=0;
-    buttonState();
-    document.getElementById('person').src='img/img/rizal.png';
-    document.getElementById('bg').src='img/img/bg_rizal.jpg';
-    quoteText.style.color = 'yellow';
-    quoteName.innerHTML = 'Jose Rizal';
-    quoteName.style.fontSize = '1.7rem';
+    buttonState = 1;
+
+    deselectButtons();
+    rizal_btn.style.filter='brightness(120%)';
+
+    bg.src='img/bg_rizal.jpg';
+    person.src='img/rizal.png';
+    person.style.top='20.5rem';
+    quote.style.color='goldenrod';
+    plate.innerHTML='Jose Rizal';
+    plate.style.fontSize='2rem';
+    quote_btn.style.visibility='visible';
 }
-function getSocQuote() {
-    if(buttonStateVar!=2){
-        quoteText.innerHTML = '';
+function setMart() {
+    if(buttonState!=2){
+        quote.innerHTML = '';
+    }
+
+    buttonState = 2;
+
+    deselectButtons();
+    mart_btn.style.filter='brightness(120%)';
+
+    bg.src='img/bg_martin.jpg';
+    person.src='img/martin.png';
+    person.style.top='20.5rem';
+    quote.style.color='white';
+    plate.innerHTML='Martin Luther<br>King Jr.';
+    plate.style.fontSize='1.36rem';
+    quote_btn.style.visibility='visible';
+}
+function setSun() {
+    if(buttonState!=3){
+        quote.innerHTML = '';
     }
     
-    buttonStateVar = 2;
-    quoteState=0;
-    buttonState();
-    document.getElementById('person').src='img/img/socrates.png';
-    document.getElementById('bg').src='img/img/bg_scorates.jpg';
-    quoteText.style.color = 'black';
-    quoteName.innerHTML = 'Socrates';
-    quoteName.style.fontSize = '1.8rem';
-}
-function getSunQuote() {
-    if(buttonStateVar!=3){
-        quoteText.innerHTML = '';
-    }
+    buttonState = 3;
 
-    buttonStateVar = 3;
-    quoteState=0;
-    buttonState();
-    document.getElementById('person').src='img/img/sun_tzu.png';
-    document.getElementById('bg').src='img/img/bg_sun_tzu.jpg';
-    quoteText.style.color = 'white';
-    quoteName.innerHTML = 'Sun Tzu';
-    quoteName.style.fontSize = '1.8rem';
-}
-function getNietQuote() {
-    if(buttonStateVar!=4){
-        quoteText.innerHTML = '';
-    }
+    deselectButtons();
+    sun_btn.style.filter='brightness(120%)';
 
-    buttonStateVar = 4;
-    quoteState=0;
-    buttonState();
-    document.getElementById('person').src='img/img/nietzsche.png';
-    document.getElementById('bg').src='img/img/bg_nietzsche.jpg';
-    quoteText.style.color = 'yellow';
-    quoteName.innerHTML = 'Nietzsche';
-    quoteName.style.fontSize = '1.7rem';
+    bg.src='img/bg_sun_tzu.jpg';
+    person.src='img/sun_tzu.png';
+    person.style.top='18rem';
+    quote.style.color='white';
+    plate.innerHTML='Sun Tzu';
+    plate.style.fontSize='2rem';
+    quote_btn.style.visibility='visible';
 }
-function getMartinQuote() {
-    if(buttonStateVar!=5){
-        quoteText.innerHTML = '';
+function setSoc() {
+    if(buttonState!=4){
+        quote.innerHTML = '';
     }
+    
+    buttonState = 4;
+    
+    deselectButtons();
+    soc_btn.style.filter='brightness(120%)';
 
-    buttonStateVar = 5;
-    quoteState=0;
-    buttonState();
-    document.getElementById('person').src='img/img/martin.png';
-    document.getElementById('bg').src='img/img/bg_martin.jpg';
-    quoteText.style.color = 'white';
-    quoteName.innerHTML = 'Martin Luther<br>King Jr.';
-    quoteName.style.fontSize = '1.1rem';
-    quoteState -= 1;
+    bg.src='img/bg_scorates.jpg';
+    person.src='img/socrates.png';
+    person.style.top='18rem';
+    quote.style.color='black';
+    plate.innerHTML='Socrates';
+    plate.style.fontSize='2rem';
+    quote_btn.style.visibility='visible';
+}
+function setNiet() {
+    if(buttonState!=5){
+        quote.innerHTML = '';
+    }
+    
+    buttonState = 5;
+
+    deselectButtons();
+    niet_btn.style.filter='brightness(120%)';
+
+    bg.src='img/bg_nietzsche.jpg';
+    person.src='img/nietzsche.png';
+    person.style.top='18rem';
+    quote.style.color='goldenrod';
+    plate.innerHTML='Nietzsche';
+    plate.style.fontSize='2rem';
+    quote_btn.style.visibility='visible';
 }
 function getQuote() {
     quoteState += 1;
@@ -76,70 +109,59 @@ function getQuote() {
         quoteState=0;
     }
 
-    switch(buttonStateVar) {
+    switch(buttonState) {
         default:
             break;
         case 1:
-            quoteText.innerHTML = rizal[quoteState];
+            quote.innerHTML = arr_rizal[quoteState];
             break;
         case 2:
-            quoteText.innerHTML = socrates[quoteState];
+            quote.innerHTML = arr_mart[quoteState];
             break;
         case 3:
-            quoteText.innerHTML = sun_tzu[quoteState];
+            quote.innerHTML = arr_sun[quoteState];
             break;
         case 4:
-            quoteText.innerHTML = nietzsche[quoteState];
+            quote.innerHTML = arr_soc[quoteState];
             break;
         case 5:
-            quoteText.innerHTML = martin[quoteState];
+            quote.innerHTML = arr_niet[quoteState];
             break;
     }
 }
-function buttonState() {
-    switch(buttonStateVar) {
-        case 0:
-            button_getQuote.disabled = true;
-            break;
-        default:
-            button_getQuote.disabled = false;
-            break;
-    }
+function deselectButtons() {
+    rizal_btn.style.filter='grayscale(100%)';
+    mart_btn.style.filter='grayscale(100%)';
+    sun_btn.style.filter='grayscale(100%)';
+    soc_btn.style.filter='grayscale(100%)';
+    niet_btn.style.filter='grayscale(100%)';
 }
 
-let button_getQuote = document.getElementById('quote_btn');
-let quoteText = document.getElementById('quote');
-let quoteName = document.getElementById('name');
-
-let buttonStateVar = 0;
-let quoteState = -1;
-buttonState();
-
-const rizal = ['He who does not know how to look back at where he came from will never get to his destination.',
-'One only dies once, and if one does not die well, a good opportunity<br>is lost and will not present<br>itself again.',
+const arr_rizal = ['He who does not know how to look back at where he came from will never get to his destination.',
+'One only dies once, and if one does not die well, a good opportunity is lost and will not present itself again.',
 'There can be no tyrants where there are no slaves.',
 'Each one writes history according to his convenience.',
 'He who would love much has also much to suffer.']
 
-const socrates = ['The only true wisdom is in knowing you know nothing.',
-'Be slow to fall into friendship, but when you are in, continue<br>firm and constant.',
-'Sometimes you put walls up not to keep people out, but to see<br>who cares enough to<br>break them down.',
-'The secret of happiness, you see, is not found in seeking more, but in developing the capacity<br>to enjoy less.',
+const arr_soc = ['The only true wisdom is in knowing you know nothing.',
+'Be slow to fall into friendship, but when you are in, continue firm and constant.',
+'Sometimes you put walls up not to keep people out, but to see who cares enough to break them down.',
+'The secret of happiness, you see, is not found in seeking more, but in developing the capacity to enjoy less.',
 'Let him who would move the world first move himself.']
 
-const sun_tzu = ['In the midst of chaos, there is also opportunity.',
+const arr_sun = ['In the midst of chaos, there is also opportunity.',
 'The greatest victory is that which requires no battle.',
 'Supreme excellence consists of breaking the enemy\'s resistance without fighting.',
 'Who wishes to fight must first count the cost.',
 'Let your plans be dark and impenetrable as night, and when you move, fall like a thunderbolt']
 
-const nietzsche = ['Without music, life would be a mistake.',
+const arr_niet = ['Without music, life would be a mistake.',
 'That which does not kill us makes us stronger.',
 'You must have chaos within you to give birth to a dancing star.',
 'In heaven, all the interesting people are missing.',
 'There are no facts, only interpretations']
 
-const martin = ['We must accept finite disappointment, but never lose infinite hope.',
+const arr_mart = ['We must accept finite disappointment, but never lose infinite hope.',
 'Forgiveness is not an occasional act. It is a permanent attitude.',
 'Never succumb to the temptation of bitterness.',
 'Let no man pull you so low as to hate him.',
